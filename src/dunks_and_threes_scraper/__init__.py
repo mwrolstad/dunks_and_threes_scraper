aba543abd0a51af0e42e6eaea9fbd44d24b47987
Mason Wrolstad <mwrolstad@plaid.com>
2024-12-25 21:35:51 -0800

    updating

--- src/dunks_and_threes_scraper/__init__.py
+++ src/dunks_and_threes_scraper/__init__.py
@@ -6,7 +6,7 @@ import re
 
 from typing import Dict
 
-JSON_REGEX_EPM = re.compile("stats:(.*),k")
+JSON_REGEX_EPM = re.compile("stats:(.*),season:")
 JSON_REGEX = re.compile("stats:(.*),ks")
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0"}
 
@@ -16,9 +16,10 @@ def parse_epm(html: str):
         json_str = (
             j[0]
             .strip()
-            .replace("z:", '"z":')
-            .replace("pctl:", '"pctl":')
-            .replace("rk:", '"rk":')
+            .replace(":", '":')
+            .replace(",", ',"')
+            .replace("{", '{"')
+            .replace( '"{"', '{"')
             .replace(",.", ",0.")
             .replace(",-.", ",-0.")
             .replace(":.", ":0.")
@@ -27,68 +28,26 @@ def parse_epm(html: str):
         json_list = json.loads(json_str)
         json_df = pd.DataFrame(json_list)
         json_df.columns = [
-            "year",
-            "player_number",
-            "idk",
-            "idk",
-            "id",
-            "team_abrv",
-            "team",
-            "idk",
-            "player_name",
-            "idk",
-            "position",
-            "games_played",
-            "total_games",
-            "gp",
-            "idk",
-            "idk",
-            "games",
-            "idk",
-            "mpg",
-            "mpg_stats",
-            "epm_off",
-            "epm_def",
-            "epm_tot",
-            "estimated_wins",
-            "epm_off_stats",
-            "epm_def_stats",
-            "epm_tot_stats",
-            "estimated_wins_stats",
-            "usage",
-            "true_shooting_%",
-            "efg_%",
-            "rim_%",
-            "mid_%",
-            "idk",
-            "3p_%",
-            "ft_%",
-            "offensive_rebound_%",
-            "defensive_rebound_%",
-            "assist_%",
-            "turnover_%",
-            "steal_%",
-            "block_%",
-            "usage_stats",
-            "true_shooting_stats",
-            "efg_stats",
-            "rim_stats",
-            "mid_stats",
-            "idk",
-            "3p_stats",
-            "ft_stats",
-            "offensive_rebound_stats",
-            "defensive_rebound_stats",
-            "assist_stats",
-            "turnover_stats",
-            "steal_stats",
-            "block_stats",
-            "idk",
-            "idk",
-            "idk",
-            "idk",
-            "idk",
+            'season', 'game_dt', 'player_id', 'player_name', 'team_id',
+            'team_abrv', 'age', 'inches', 'weight', 'rookie_year', 'position',
+            'epm_off', 'epm_def', 'epm_tot', 'idk', 'idk', 'mpg', 'usage',
+            'ppg', 'idk', 'efg_%"', 'rim_at', 'mid_at',
+            '2p_at', '3p_at', 'ft_at', 'rim_%', 'mid_%',
+            '2p_%', '3p_%', 'ft_%', 'ast', 'tov',
+            'orb', 'drb', 'stl', 'blk', 'idk', 'idk',
+            'idk', 'idk', 'idk', 'idk', 'idk',
+            'idk', 'idk', 'idk',
+            'idk', 'idk', 'idk', 'idk',
+            'idk', 'idk', 'idk', 'idk',
+            'idk', 'idk', 'idk', 'idk',
+            'idk', 'idk', 'idk', 'idk', 'mpg_z', 'usage_z',
+            'ppg_z', 'idk', 'efg_%_z"', 'rim_at_z', 'mid_at_z',
+            '2p_at_z', '3p_at_z', 'ft_at_z',
+            'rim_%_z', 'mid_%_z', '2p_%_z', '3p_%_z', 'ft_%_z', 'ast_z', 'tov_z',
+            'orb_z', 'drb_z', 'stl_z', 
+            'blk_z', 'epm_off_z', 'epm_def_z', 'epm_tot_z'
         ]
+        json_df.drop("idk", axis=1, inplace=True)
         return json_df.to_dict(orient="records")
     else:
         return []
@@ -168,14 +127,17 @@ def parse_html(html: str):
 
 
 def scrape_stats(year: str, proxies: Dict[str, str] = None):
-    url_template = "https://dunksandthrees.com/?season={year}"
+    # url_template = "https://dunksandthrees.com/?season={year}"
+    url_template = "https://dunksandthrees.com/stats/team?season={year}"
 
     r = requests.get(url=url_template.format(year=year), headers=HEADERS, proxies=proxies)
     return parse_html(r.text)
 
 
-def scrape_epm(year: str, proxies: Dict[str, str] = None):
-    url_template = "https://dunksandthrees.com/epm?season={year}"
+def scrape_epm(year: str = None, proxies: Dict[str, str] = None):
+    url_template_base = "https://dunksandthrees.com/epm"
+    url_template = f"{url_template_base}?season={year}" if year else url_template_base
+    print(f"Processing {url_template}")
 
     r = requests.get(url=url_template.format(year=year), headers=HEADERS, proxies=proxies)
     return parse_epm(r.text)
@@ -210,7 +172,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Crawl dunks and threes website for a specifc year and get data.")
     parser.add_argument(
         "--year",
-        required=True,
+        required=False,
+        default=None,
         help="The year number to crawl...",
     )
     parser.add_argument(
